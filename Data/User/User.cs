df72a7625e01cd8e34e5586d16d4bcc2d8f54e46
CrisDaniel25 <63526691+CrisDaniel25@users.noreply.github.com>
2021-01-30 17:17:58 -0400

    [ADD Project]

---
+++ Data/User/User.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea_2.Data
+{
+    public class User
+    {
+        public string gender {get; set;}
+        public Name name {get; set;}
+    }
+}
