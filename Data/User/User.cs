7c88e25bca69d2aa02b260b04b62754d5d16e4ed
CrisDaniel25 <63526691+CrisDaniel25@users.noreply.github.com>
2021-02-01 19:35:15 -0400

    [UPDATE]

--- Data/User/User.cs
+++ Data/User/User.cs
@@ -6,7 +6,19 @@ namespace Tarea_2.Data
 {
     public class User
     {
+       public Result results {get; set;}
+    }
+
+    public class Result
+    {
         public string gender {get; set;}
         public Name name {get; set;}
+    }
+
+    public class Name 
+    {
+        public string title {get; set;}
+        public string first {get; set;}
+        public string last {get; set;}
     }
 }
